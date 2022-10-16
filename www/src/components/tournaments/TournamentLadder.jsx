fe9ffbefa1e99c8724b539831d2c9c52a69f08f3
Jesse Berger <admin@jesseberger.me>
2022-10-15 21:53:56 -0500

    add ladder header styling

--- www/src/components/tournaments/TournamentLadder.jsx
+++ www/src/components/tournaments/TournamentLadder.jsx
@@ -4,78 +4,17 @@ import TournamentPlayerSummary from "./TournamentPlayerSummary";
 import { Form, Button, Col, FloatingLabel, Row } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 
-// const tournamentPlayersDoc = `
-// query AllTournamentPlayers($tournament_id: uuid = "") {
-//   TournamentPlayer(where: {Tournament: {id: {_eq: $tournament_id}}}) {
-//     player_list_id
-//     rank
-//     player_name
-//     mov
-//     loss
-//     win
-//     tournament_points
-//     sos
-//     club
-//     group
-//     id
-//     tournament_id
-//     user_id
-//     User {
-//       name
-//     }
-//   }
-// }
-// `;
-
 export default function Ladder(props) {
-    // const [tournamentPlayers, setTournamentPlayers] = useState([]);
-    // const { getAccessTokenSilently } = useAuth0();
-    // const {filter } = props
-    // function usePrevious(value) {
-    //     const ref = useRef();
-    //     useEffect(() => {
-    //       ref.current = value;
-    //     });
-    //     return ref.current;
-    // }
-    // const prevProps = usePrevious({filter});
-    // useEffect(() => {
-    //     getTournamentPlayers();
-    // },[])
-
-    // useEffect(() => {
-    //     if (prevProps == undefined || props.filter !== prevProps.filter) {
-    //         getTournamentPlayers();
-    //     }
-    // },[props, prevProps])
-
-  // const getTournamentPlayers = async () => {
-  //   const accessToken = await getAccessTokenSilently()
-  //   if (!tournamentPlayers) {
-  //     console.log("Aborting GetTournamentPlayers... no tournamentPlayers.");
-  //     return;
-  //   }
-
-  //   Query("AllTournamentPlayers", tournamentPlayersDoc, {
-  //     tournament_id: props.tournament.id,
-  //   },accessToken).then((response) => {
-  //     if (response){ //Why is response undefined?
-  //       setTournamentPlayers(response.TournamentPlayer)
-  //     }
-  //   });
-  // }
-
     if (props.Ladder) {
       console.log("Got past the state check...");
       return (
         <div>
-          <Row className="pb-3">
+          <Row className="pb-1 header mb-3">
             <Col>Player</Col>
             <Col>Rank</Col>
+            <Col>W/L</Col>
             <Col>TP</Col>
             <Col>MoV</Col>
-            <Col>Wins</Col>
-            <Col>Losses</Col>
             <Col>Club</Col>
           </Row>
           {props.Ladder.map((player) => (
--- www/src/components/tournaments/TournamentPlayerSummary.jsx
+++ www/src/components/tournaments/TournamentPlayerSummary.jsx
@@ -9,10 +9,9 @@ export default function TournamentPlayerSummary(props) {
         {console.log(props.player.player_name)}
         <Col>{props.player.player_name}</Col>
         <Col>{props.player.rank}</Col>
+        <Col>{props.player.win} / {props.player.loss}</Col>
         <Col>{props.player.tournament_points}</Col>
         <Col>{props.player.mov}</Col>
-        <Col>{props.player.win}</Col>
-        <Col>{props.player.loss}</Col>
         <Col>{props.player.club}</Col>
       </Row>
     );
