a016be15726c834e7f66845c825665fa86ba20b1
Jesse Berger <admin@jesseberger.me>
2022-10-18 01:37:37 -0500

    filtered games in result submission tab

--- www/src/components/tournaments/TournamentResultSubmission.jsx
+++ www/src/components/tournaments/TournamentResultSubmission.jsx
@@ -5,16 +5,19 @@ import {TournamentHomeContext} from "../../pages/tournaments/TournamentHome"
 
 export default function TournamentResultSubmission() {
     const {rounds, ladder, tournament, updateTournament, isOwner} = useContext(TournamentHomeContext);
+    const { user, getAccessTokenSilently } = useAuth0();
 
+    const isMine = (match) => match.Players.map(mp => mp.User?.id).includes(user?.sub)
+    const roundHasMyGame = (round) => round.Matches.map(m => isMine(m)).reduce((a,b)=>a||b,false)
     //Todo, filter matches on logged in user?
     return (
         <>
-        {rounds.map(r => <>
-            <div>Round {r.round_num} </div>
-            {r.Matches.map(r => <>
-                <div>Match</div>
-            </>)}
-        </>)}
+        {rounds.map(r => <div key={r.id}>
+            {roundHasMyGame(r)?<div>Round {r.round_num} </div>:<></>}
+            {r.Matches.map(m => <div key={m.id}>
+                {isMine(m)?<div>My Round</div>:<></>}
+            </div>)}
+        </div>)}
         </>
     );
 }
