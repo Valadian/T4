8fa71c9af83bca3dfaceb6a6e060af01864836d6
sprint_ska <sprint_ska@hotmail.com>
2022-10-19 01:40:55 -0700

    Pull in env vars to use planting seeds

--- dev-utils/plant_seeds.sh
+++ dev-utils/plant_seeds.sh
@@ -4,12 +4,16 @@ export SEEDED_TABLES=("User" "Tournament" "ScoringRuleset") # Names of non-enum 
 
 export DEV_UTILS_FOLDER="${0%/*}"
 
+set -a
+source <(cat $DEV_UTILS_FOLDER/../.env | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
+set +a
+
 cd "$DEV_UTILS_FOLDER/../hasura-data" || exit
 
 for TABLE in ${SEEDED_TABLES[@]}; do
     echo "Planting $TABLE seeds..."
     find . -name "*"$TABLE"Seed.sql" | xargs -I PpP basename PpP | while read -r SEEDFILE; do
-        hasura seed apply --database-name default -f $SEEDFILE --admin-secret $HASURA_GRAPHQL_ADMIN_SECRET || echo "Error applying "$SEEDFILE" seed; seed data may be incomplete.  Admin secret was "$HASURA_GRAPHQL_ADMIN_SECRET
+        hasura seed apply --database-name default -f $SEEDFILE --admin-secret $HASURA_GRAPHQL_ADMIN_SECRET || echo "Error applying "$SEEDFILE" seed; seed data may be incomplete."
     done
     echo "-"
 done
