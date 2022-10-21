b70860f9b0525e978c28bacfbca8811ea1de325c
sprint_ska <sprint_ska@hotmail.com>
2022-10-21 16:29:11 -0700

    Add Hasura URL env var to React App

--- www/src/data/T4GraphContext.js
+++ www/src/data/T4GraphContext.js
@@ -1,39 +1,38 @@
-async function Query(operationName, operationDoc,variables,accessToken) {
-    var headers = {
-        'Content-Type': 'application/json'
-    }
-    if (accessToken){
-        headers['Authorization'] = `Bearer ${accessToken}`
-    }
-    return fetch(
-        "http://localhost:8080/v1/graphql",
-        {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify({
-                query: operationDoc,
-                variables: variables??{},
-                operationName: operationName
-            })
-        }
-    )
+async function Query(operationName, operationDoc, variables, accessToken) {
+  var headers = {
+    "Content-Type": "application/json",
+  };
+  if (accessToken) {
+    headers["Authorization"] = `Bearer ${accessToken}`;
+  }
+  graph_url = process.env.REACT_APP_GRAPH_URL;
+
+  return fetch(graph_url, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({
+      query: operationDoc,
+      variables: variables ?? {},
+      operationName: operationName,
+    }),
+  })
     .then((response) => response.json())
     .then((json) => {
-        if(json.errors && json.errors.length>0){
-            // console.error(json.errors);
-            // throw new Error("Graphql query has error, see console")
-            return Promise.reject(json.errors)
-        }
-        if (json.data) {
+      if (json.errors && json.errors.length > 0) {
+        // console.error(json.errors);
+        // throw new Error("Graphql query has error, see console")
+        return Promise.reject(json.errors);
+      }
+      if (json.data) {
         // console.log(json.data);
-            return Promise.resolve(json.data)
-        } else {
-            return Promise.reject("No Data Returned")
-        }
+        return Promise.resolve(json.data);
+      } else {
+        return Promise.reject("No Data Returned");
+      }
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
 
-export default Query
+export default Query;
