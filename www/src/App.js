221f0456489b39da27114289c64bb4ae20a2b1e9
ardaedhel <sprint_ska@hotmail.com>
2022-10-30 07:09:05 +0000

    Reenable TournamentEditor

--- www/src/App.js
+++ www/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
@@ -6,7 +6,7 @@ import Events from "./pages/Events";
 import Players from "./pages/Players";
 import PlayerEditor from "./pages/players/PlayerEditor";
 import TournamentEditor from "./pages/tournaments/TournamentEditor";
-import {TournamentHome} from "./pages/tournaments/TournamentHome";
+import { TournamentHome } from "./pages/tournaments/TournamentHome";
 import Search from "./pages/Search";
 import Profile from "./pages/Profile";
 import NoPage from "./pages/NoPage";
@@ -19,6 +19,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="events" element={<Events />} />
         <Route path="events/:id" element={<TournamentHome />} />
+        <Route path="events/add/" element={<TournamentEditor />} />
         <Route path="events/edit/:id" element={<TournamentEditor />} />
         <Route path="players" element={<Players />} />
         <Route path="players/:id" element={<PlayerEditor />} />
@@ -27,7 +28,7 @@ function App() {
         <Route
           path="/profile"
           element={<ProtectedRoute path="profile" component={Profile} />}
-          />
+        />
         <Route path="*" element={<NoPage />} />
       </Route>
     </Routes>
