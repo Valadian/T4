645cd41c48e9f953e84781721ad9f15bd02f231e
ardaedhel <sprint_ska@hotmail.com>
2022-10-31 05:46:44 +0000

    Resolve #15

--- www/src/components/tournaments/TournamentLadderToggle.jsx
+++ www/src/components/tournaments/TournamentLadderToggle.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useContext }  from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Query from "../../data/T4GraphContext";
 import { Form, Button, Col, FloatingLabel, Row } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import { useAuth0 } from "@auth0/auth0-react";
-import {TournamentHomeContext} from "../../pages/tournaments/TournamentHome"
+import { TournamentHomeContext } from "../../pages/tournaments/TournamentHome";
 
 const updateLadderVisibilityDoc = `
   mutation UpdateLadderVisibility($ladder_visible: Boolean, $tournament_id: uuid = "") {
@@ -17,50 +17,56 @@ const updateLadderVisibilityDoc = `
 `;
 
 export default function TournamentLadderToggle(props) {
-    const [isLadderVisible, setIsLadderVisible] = useState(false);
-    const { getAccessTokenSilently } = useAuth0();
-    const {tournament, updateTournament} = useContext(TournamentHomeContext);
+  const [isLadderVisible, setIsLadderVisible] = useState(false);
+  const { getAccessTokenSilently } = useAuth0();
+  const { tournament, updateTournament } = useContext(TournamentHomeContext);
 
-    const updateLadderVisibility = async () => {
-        if (!props || !tournament.id) {
-            return;
-        }
-        // Almost certain this was a bug:
-        //let ladderIsVisible = !tournament.ladder_visible;
-        let ladderIsVisible = tournament.ladder_visible;
-
-        const accessToken = await getAccessTokenSilently()
-        Query("UpdateLadderVisibility", updateLadderVisibilityDoc, {
-            ladder_visible: ladderIsVisible,
-            tournament_id: tournament.id,
-        },accessToken)
-        .then((data) => setIsLadderVisible( data.ladder_visible ))
-        .then(() => updateTournament());
-    };
-
-    const handleClose = () => {
-        props.show = false;
-    };
-
-    //console.log(`Got here with... ${JSON.stringify(props)}`);
-    if (props) {
-      return (
-        <Button
-          variant="outline-primary"
-          onClick={updateLadderVisibility}
-          size="sm"
-        >
-          <i className="bi bi-list-ol text-primary"></i>{" "}
-          <span className="text-white">{props.button_text}</span>
-        </Button>
-      );
-    } else {
-      return (
-        <>
-          <div>Loading...</div>
-        </>
-      );
+  const updateLadderVisibility = async () => {
+    if (!props || !tournament.id) {
+      return;
     }
+    // Almost certain this was a bug:
+    //let ladderIsVisible = !tournament.ladder_visible;
+    // Not a bug, it's a toggle.
+    let ladderIsVisible = !tournament.ladder_visible;
+
+    const accessToken = await getAccessTokenSilently();
+    Query(
+      "UpdateLadderVisibility",
+      updateLadderVisibilityDoc,
+      {
+        ladder_visible: ladderIsVisible,
+        tournament_id: tournament.id,
+      },
+      accessToken
+    )
+      .then((data) => setIsLadderVisible(data.ladder_visible))
+      .then(() => updateTournament());
+  };
+
+  const handleClose = () => {
+    props.show = false;
+  };
+
+  //console.log(`Got here with... ${JSON.stringify(props)}`);
+  if (props) {
+    return (
+      <Button
+        variant="outline-primary"
+        onClick={updateLadderVisibility}
+        size="sm"
+      >
+        <i className="bi bi-list-ol text-primary"></i>{" "}
+        <span className="text-white">{props.button_text}</span>
+      </Button>
+    );
+  } else {
+    return (
+      <>
+        <div>Loading...</div>
+      </>
+    );
+  }
 }
 
 /*{
