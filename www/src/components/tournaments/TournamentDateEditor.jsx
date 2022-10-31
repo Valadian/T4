82ad9f09d9c1e8503beb38488fe0baf76dfa1537
ardaedhel <sprint_ska@hotmail.com>
2022-10-31 05:02:59 +0000

    Resolve #13

--- www/src/components/tournaments/TournamentDateEditor.jsx
+++ www/src/components/tournaments/TournamentDateEditor.jsx
@@ -5,7 +5,7 @@ import Modal from "react-bootstrap/Modal";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useAuth0 } from "@auth0/auth0-react";
-import {TournamentHomeContext} from "../../pages/tournaments/TournamentHome"
+import { TournamentHomeContext } from "../../pages/tournaments/TournamentHome";
 
 const updateTournamentDateDoc = `
   mutation UpdateTournamentDate($tournament_id: uuid = "", $start: date = "") {
@@ -19,69 +19,70 @@ const updateTournamentDateDoc = `
 `;
 
 export default function TournamentDateEditor(props) {
-    const {tournament, updateTournament} = useContext(TournamentHomeContext);
-    const [newTournamentDate, setNewTournamentDate] = useState(tournament.start);
-    const { getAccessTokenSilently } = useAuth0();
+  const { tournament, updateTournament } = useContext(TournamentHomeContext);
+  const [newTournamentDate, setNewTournamentDate] = useState(tournament.start);
+  const { getAccessTokenSilently } = useAuth0();
 
-    const updateTournamentDate = async (new_tournament_date) => {
-        if (!(tournament?.id)) {
-        return;
-        }
+  const updateTournamentDate = async (new_tournament_date) => {
+    if (!tournament?.id) {
+      return;
+    }
 
-        const id = tournament.id;
+    const id = tournament.id;
 
-        const accessToken = await getAccessTokenSilently()
-        Query("UpdateTournamentDate", updateTournamentDateDoc, {
-        start: newTournamentDate,
+    const accessToken = await getAccessTokenSilently();
+    Query(
+      "UpdateTournamentDate",
+      updateTournamentDateDoc,
+      {
+        start: new_tournament_date,
         tournament_id: id,
-        },accessToken).then((data) =>
-        // TODO: insert a "success" toast
-        setNewTournamentDate(data.start)
-        );
-        updateTournament();
-    }
+      },
+      accessToken
+    ).then((data) =>
+      // TODO: insert a "success" toast
+      setNewTournamentDate(data.start)
+    );
+    updateTournament();
+  };
 
-    const handleClose = () => {
-        props.show = false;
-    };
+  const handleClose = () => {
+    props.show = false;
+  };
+  console.log(tournament);
 
-
-    if (props && tournament && newTournamentDate) {
-      return (
-        <Modal
-          onHide={props.onHide}
-          size="sm"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-          show={props.show}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              Update Event Date
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Row>
-              <Col className="d-flex">
-                <DatePicker
-                  selected={newTournamentDate}
-                  onChange={updateTournamentDate}
-                  className=""
-                  inline
-                />
-              </Col>
-            </Row>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={props.onHide}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      );
-    } else {
-      return (
-        <>
-          <div>Loading...</div>
-        </>
-      );
-    }
+  if (tournament && newTournamentDate) {
+    return (
+      <Modal
+        onHide={props.onHide}
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={props.show}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Update Event Date
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row>
+            <Col className="d-flex">
+              <DatePicker
+                selected={newTournamentDate}
+                onChange={updateTournamentDate}
+                className=""
+                inline
+              />
+            </Col>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
