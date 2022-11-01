e3519ed7d3e125b5b5ad6d2bd4da55ae6e927244
Jesse Berger <admin@jesseberger.me>
2022-10-31 22:16:20 -0500

    Made login button more obvious

--- www/src/auth/LoginButton.jsx
+++ www/src/auth/LoginButton.jsx
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button className="btn btn-primary" onClick={() => loginWithRedirect()} title="Sign in"><i className="bi bi-box-arrow-in-right"></i></button>;
+  return <button className="btn btn-primary" onClick={() => loginWithRedirect()} title="Sign in">Login <i className="bi bi-box-arrow-in-right"></i></button>;
 };
 
 export default LoginButton;
