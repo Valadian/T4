572da04af04f75632d479d4ce8498531f1fd7434
Jesse Berger <admin@jesseberger.me>
2022-11-02 00:22:57 -0500

    add ga

--- www/src/index.js
+++ www/src/index.js
@@ -10,12 +10,25 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/Auth0Provider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const injectGA = () => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SFHLRBRV9T');
+}
 root.render(
+  <>
+  {/* Google tag (gtag.js) */}
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SFHLRBRV9T"></script>
+  <script>{injectGA()}</script>
+
   <Router>
     <Auth0ProviderWithHistory>
       <App />
     </Auth0ProviderWithHistory>
   </Router>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
