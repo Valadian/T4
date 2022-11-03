8cdfd7a48783a250d2d2d0c9c3523e7c66b81c8b
Jesse Berger <admin@jesseberger.me>
2022-11-02 19:16:12 -0500

    moving meta/ga to index instead of helmet

--- www/src/index.js
+++ www/src/index.js
@@ -8,34 +8,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/Auth0Provider";
-import {Helmet} from "react-helmet";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <Helmet>
-    {/* Global site tag (gtag.js) - Google Analytics  */}
-    <script async src='https://www.googletagmanager.com/gtag/js?id=G-SFHLRBRV9T'></script>
-    <script>
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-SFHLRBRV9T');
-      `}
-    </script>
-    <meta property="og:title" content="TableTop Tournament Tools (T4)" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://t4.tools" />
-    <meta property="og:image" content="https://t4.tools/favicon.ico" />
-    <meta property="og:description" content="Toolset for managing Tabletop Tournament Games" />
-    <meta name="twitter:title" content="TableTop Tournament Tools (T4) " />
-    <meta name="twitter:description" content="Toolset for managing Tabletop Tournament Games" />
-    <meta name="twitter:image" content="https://t4.tools/favicon.ico" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff" />
-    <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#222222" />
-  </Helmet>
 
   <Router>
     <Auth0ProviderWithHistory>
